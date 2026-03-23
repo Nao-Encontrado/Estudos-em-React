7254cb52a08da4f9a6b156b8557d70a61ce2e99d
404 <zionkeim387@gmail.com>
2026-03-22 22:08:32 -0300

    primeiro commit

---
+++ meu-primeiro-app/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+function App() {
+  const [tarefa, setTarefa] = useState("")
+  const [tarefas, setTarefas] = useState([])
+  const [contador, setContador] = useState(0)
+
+  function adicionarTarefa() {
+    if (tarefa === "") return
+    setTarefas([...tarefas, tarefa])
+    setTarefa("")
+  }
+
+  function AddContador() {
+    if (contador === 10) {
+      return
+    }
+    setContador(contador + 1)
+  }
+
+  function SubContador() {
+    if (contador === 0) {
+      return
+    }
+    setContador(contador - 1)
+  }
+
+  return (
+    <div>
+      <h2>Digite uma tarefa:</h2>
+      <input
+        value={tarefa}
+        onChange={(e) => setTarefa(e.target.value)}
+      />
+      <button onClick={adicionarTarefa}>
+        Adicionar
+      </button>
+
+      <ul>
+        {tarefas.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+
+      <div>
+        <h2>{contador}</h2>
+        <button onClick={AddContador}>+</button>
+        <button onClick={SubContador}>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
